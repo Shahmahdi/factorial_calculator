cc2906513167380761aa20482fa45468ed0aa753
mahdi <shahmahdi11@gmail.com>
2020-02-04 00:02:17 +0600

    factorial calculator done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 const App = () => {
+
+  const [enteredNumber, setEnteredNumber] = useState(undefined as any);
+  const [error, setError] = useState('');
+  const [result, setResult] = useState(0);
+
+  const onSubmit = () => {
+    let result = 1;
+    for (let i = 2; i <= enteredNumber; i++) {
+      result = result * i;
+    }
+    setResult(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column items-center pt0 pt6-ns vh-100">
+      <h1>Factorial Calculator</h1>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          onSubmit();
+        }}
+      >
+        {error !== '' ? <p className="f6 red">{error}</p> : undefined}
+        <input 
+          type="number" 
+          placeholder="Enter a number..." 
+          onChange={e => {
+            if (parseInt(e.target.value) < 0) {
+              setError('Number must be > or = to 0');
+            } else {
+              setError('');
+              setEnteredNumber(e.target.value)
+            }
+          }}
+        />
+        <br />
+        <button className="mt1">Calculate Factorial</button>
+      </form>
+      <h2>Factorial: {result}</h2>
     </div>
   );
 }
